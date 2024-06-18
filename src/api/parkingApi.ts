ec4f77669cbf0454a384caa5523ec2baa7c2b501
jandres-vega <jandresvega@itsa.edu.co>
2024-06-18 12:04:35 -0500

    Add URL production

--- src/api/parkingApi.ts
+++ src/api/parkingApi.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
-//https://f75f-181-59-220-38.ngrok-free.app
 //http://localhost:3000/api/v1/
 const parkingApi = axios.create({
-  baseURL: 'https://9ecc-181-59-220-38.ngrok-free.app/api/v1',
+  baseURL: 'https://api-ceet.onrender.com/api/v1',
 });
 
 export {parkingApi};
